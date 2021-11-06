76a59c1ec69dc53ad5ee2e15512ea4a83530c8c1
srikanthpragada <srikanthpragada@gmail.com>
2021-11-06 11:05:03 +0530

    06-NOV

---
+++ CsharpDemo/Factors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo
+{
+    class Factors
+    {
+        static void Main()
+        {
+            int num, i;
+
+            Console.Write("Enter a number :");
+            num = Int32.Parse(Console.ReadLine()); // Convert string to int
+
+            for (i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                    Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
