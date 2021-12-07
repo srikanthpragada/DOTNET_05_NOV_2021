8fe8cdf0ee1a347124291ad19ba2e4786329e407
srikanthpragada <srikanthpragada@gmail.com>
2021-12-07 08:57:37 +0530

    07-DEC

---
+++ CsharpDemo/EF/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CsharpDemo.EF
+{
+    [Table("categories")]
+    class Category
+    {
+        [Column("CatCode")]
+        [Key]
+        public string Code { get; set; }
+
+        [Column("CatDesc")]
+        public string Description { get; set; }
+    }
+}
---
+++ CsharpDemo/EF/ListCategories.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo.EF
+{
+    class ListCategories
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new InventoryContext();
+            // EF Log to console 
+            ctx.Database.Log = Console.WriteLine;
+
+            // List all categories 
+            foreach (var cat in ctx.Categories)
+                Console.WriteLine($"{cat.Code}  - {cat.Description}");
+
+            // List categories where code starts with m
+            foreach (var cat in ctx.Categories.Where(c => c.Code.StartsWith("m")))
+                Console.WriteLine($"{cat.Code}  - {cat.Description}");
+
+            var selcats = from c in ctx.Categories
+                          where c.Code.StartsWith("u")
+                          select c;
+
+            foreach (var cat in selcats)
+                Console.WriteLine($"{cat.Code}  - {cat.Description}");
+
+        }
+    }
+}
