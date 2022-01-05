110de013e698593cd741b2708214010b5b18ae6f
srikanthpragada <srikanthpragada@yahoo.com>
2022-01-05 08:56:48 +0530

    05-JAN

---
+++ Contacts/Controllers/ContactsController.cs
@@ -0,0 +1,155 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Contacts.Data;
+using Contacts.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Contacts.Controllers
+{
+    [Authorize]
+    public class ContactsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ContactsController(ApplicationDbContext context,
+             UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Contacts
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(HttpContext.User);
+            return View(await _context.Contacts.Where(c => c.UserId == userId).ToListAsync());
+        }
+
+
+        // GET: Contacts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(String Fullname, String Email, String Mobile)
+        {
+            // Get userid of currently logged in user 
+            var userId = _userManager.GetUserId(HttpContext.User);
+
+            var contact = new Contact
+            {
+                Fullname = Fullname,
+                Email = Email,
+                Mobile = Mobile,
+                UserId = userId
+            };
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Fullname,Email,Mobile,UserId")] Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactExists(contact.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactExists(int id)
+        {
+            return _context.Contacts.Any(e => e.Id == id);
+        }
+    }
+}
--- Contacts/Models/Contact.cs
+++ Contacts/Models/Contact.cs
@@ -8,14 +8,16 @@ namespace Contacts.Models
         public int Id { get; set; }
 
         [MaxLength(30)]
+        [Required]
         public string Fullname { get; set; }
 
         [MaxLength(50)]
+        [Required]
         public string Email { get; set; }
 
         [MaxLength(10)]
         [RegularExpression(@"\d{10}", ErrorMessage = "Invalid mobile number")]
-        public string Mobile { get; set; }
-        public string UserId { get; set; }
+        public string? Mobile { get; set; }
+        public string UserId { get; set; }  // UserId of the owner
     }
 }
