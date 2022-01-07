e2556244afec701b4e8774834dfc2f39e3bc6511
srikanthpragada <srikanthpragada@yahoo.com>
2022-01-07 08:56:04 +0530

    08-JAN

---
+++ BooksAPI/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace BooksAPI
+{
+    public class CatalogContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public string ConnectionString { get; }
+        public CatalogContext(IConfiguration Configuration)
+        {
+            ConnectionString = Configuration.GetConnectionString("SQLConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // options.LogTo(Console.WriteLine, LogLevel.Information);
+            options.UseSqlServer(ConnectionString);
+        }
+
+
+    }
+}
